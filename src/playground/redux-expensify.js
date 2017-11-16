c20d2c4271ae7a20432f73d6307af43931ec79f7
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-16 11:23:10 +0100

    completed redux store

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -127,9 +127,15 @@ const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
   return expenses.filter((expense) => {
     const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
     const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
-    const textMatch = true;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
 
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if(sortBy === 'date'){
+      return a.createdAt < b.createdAt ? 1 : -1
+    }else if(sortBy === 'amount'){ 
+      return a.amount < b.amount ? 1 : -1
+    }
   });
 }
 
@@ -150,19 +156,19 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100,
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 20, createdAt:-1000}))
 const expenseThree = store.dispatch(addExpense({ description: 'Fee', amount: 1000, createdAt:1500}))
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id}));
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
-store.dispatch(setTextFilter('rent'));
-store.dispatch(setTextFilter());
+// store.dispatch(removeExpense({ id: expenseOne.expense.id}));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+ //store.dispatch(setTextFilter('fee'));
+// store.dispatch(setTextFilter());
 
-store.dispatch(sortByAmount())
-store.dispatch(sortByDate())
-store.dispatch(sortByAmount())
+//store.dispatch(sortByAmount())
+ //store.dispatch(sortByDate())
+// store.dispatch(sortByAmount())
 
 
-store.dispatch(setStartDate(0))
-// store.dispatch(setStartDate())
-store.dispatch(setEndDate(1500))
+// store.dispatch(setStartDate(0))
+// // store.dispatch(setStartDate())
+// store.dispatch(setEndDate(1500))
 // store.dispatch(setEndDate())
 
 //Filters reducer
