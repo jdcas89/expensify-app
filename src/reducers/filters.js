507d0bdbe580247a54ba13571c4f63c63613877c
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-17 20:10:12 +0100

    added date range filter

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,9 +1,9 @@
-
+import moment from 'moment'
 const filtersReducerDefaultState = {
   text: "",
   sortBy: "date",
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
 export default (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
