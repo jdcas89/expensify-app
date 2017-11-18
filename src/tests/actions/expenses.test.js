536e6dcb904a78bf87c9f0bff8f63233ce05ec1e
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-18 10:26:56 +0100

    Added tests for expenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,9 +1,10 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses'
+import * as types from '../../actions/actionTypes';
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({id:'123qwe'});
   expect(action).toEqual({
-    type: 'REMOVE_EXPENSE',
+    type: types.REMOVE_EXPENSE,
     id: '123qwe'
   })
 })
@@ -11,7 +12,7 @@ test('should setup remove expense action object', () => {
 test('should setup edit expense action object', () => {
   const action = editExpense('123qwe', {note:'this is a note'});
   expect(action).toEqual({
-    type: 'EDIT_EXPENSE',
+    type: types.EDIT_EXPENSE,
     id: '123qwe',
     updates:{note:'this is a note'}
   })
@@ -19,6 +20,36 @@ test('should setup edit expense action object', () => {
 
 
 
-test('addExpense should have a default state', () => {
+test('addExpense action should have provided data', () => {
+  const expenseData = {
+    description:'this is a description',
+    amount: 2000,
+    createdAt:10000,
+    note:'this is a note'
+  }
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: types.ADD_EXPENSE,
+    expense:{
+      ...expenseData,
+      id: expect.any(String)
+    }
+  })
+})
 
+test('addExpense action should have default data', () => {
+  const expenseData = {
+    description:'',
+    amount: 0,
+    createdAt:0,
+    note:''
+  }
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: types.ADD_EXPENSE,
+    expense:{
+      ...expenseData,
+      id: expect.any(String)
+    }
+  })
 })
