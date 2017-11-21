255a7da20d1d27a882fd82f6aff83c13e1f28bf8
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-21 20:49:48 +0100

    added enzyme and did initial enzyme config

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow }  from 'enzyme';
 import Header from '../../components/Header';
 
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header/>)
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
 });
 
