ee3b010aac222e5b22281efb2917313ff3e1a5b7
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-21 22:18:28 +0100

    added event tests with Jest simulate

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,60 @@ test('should render ExpenseForm with expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>)
   expect(wrapper).toMatchSnapshot();
 });
+
+
+test('should render error for invalid form submit', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+    preventDefault:() => {}
+  });
+  expect(wrapper.state('errorMessage').length).toBeGreaterThan(0);
+  expect(wrapper.find('p').text()).toBe('You have to provide a description and an amount!')
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  const value = 'New Description'
+  wrapper.find('input').at(0).simulate('change', {
+    target:{
+      value: value
+    }
+  });
+  expect(wrapper.state('description')).toBe(value);
+})
+
+test('should set note on input change', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  const value = 'New text area input'
+  wrapper.find('textarea').simulate('change', {
+    target:{
+      value: value
+    },
+    persist: () => {}
+  });
+  expect(wrapper.state('note')).toBe(value);
+})
+
+test('should set amount if valid input', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  const value = '23.50'
+  wrapper.find('input').at(1).simulate('change', {
+    target:{
+      value: value
+    }
+  });
+  expect(wrapper.state('amount')).toBe(value);
+})
+
+test('should not set amount if invalid input', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  const value = '23.5000'
+  wrapper.find('input').at(1).simulate('change', {
+    target:{
+      value: value
+    }
+  });
+  expect(wrapper.state('amount')).toBe('');
+})
