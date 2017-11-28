32405ef2897fda019770bb0f81324eec7b69532d
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-28 21:53:04 +0100

    added numeral formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, note, amount, createdAt }) => (
   <div>
   <Link to={`/edit/${id}`}>  <h3>{description}</h3></Link>
    
     <p> <strong>Notes: </strong> {note}</p>
-    <p> <strong>Amount: </strong> {amount}</p>
+    <p> <strong>Amount: </strong> {numeral(amount/100).format("$0,0.00")}</p>
     <p><strong> Date created:</strong> {moment(createdAt).format('MMMM Do YYYY')}</p>
     <hr />
   </div>
