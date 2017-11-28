d8750de6e3c031b60a3ca28ef6a25ea2e4b8adbe
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-28 21:05:06 +0100

    don't know what's wrong

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,49 @@
-// entry -> output
-//https://webpack.js.org/concepts/
-
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 
 module.exports = (env) => {
   const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
+
   return {
     entry: './src/app.js',
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
-    module:{
-      rules:[{
-        loader:'babel-loader',
+    module: {
+      rules: [{
+        loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
-      },{
-        test:/\.s?css$/,
-        use:CSSExtract.extract({
-          use:[
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
             {
               loader: 'css-loader',
-              options:{
-                sourceMap:true
+              options: {
+                sourceMap: true
               }
             },
             {
               loader: 'sass-loader',
-              options:{
-                sourceMap:true
+              options: {
+                sourceMap: true
               }
             }
           ]
         })
-      }
-    ]
+      }]
     },
-    plugins:[
+    plugins: [
       CSSExtract
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
-    devServer:{
-      contentBase:path.join(__dirname, 'public'),
-      historyApiFallback:true,
-      publicPath:'/dist/'
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
-  }
-}
+  };
+};
