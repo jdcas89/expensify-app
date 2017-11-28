de6cd96a59914a8991021d710c350370788d46f9
Juan David Castellanos <juandavid.castellanos@live.com>
2017-11-28 08:42:19 +0100

    server ready for production

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 const app = express();
 const publicPath = path.join(__dirname,'..', 'public')
@@ -10,6 +11,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up and running...')
 })
