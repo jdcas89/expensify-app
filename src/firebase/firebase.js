e507b0fc439b15e59fa04dd97d6c86c4df32604c
Juan David Castellanos <juandavid.castellanos@live.com>
2017-12-08 08:36:41 +0100

    added firebase test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import moment from 'moment';
 // https://firebase.google.com/docs/reference/js/
 
 const config = {
-  apiKey: "AIzaSyB6_W5V2gDNyM6CFpp_kNdEfs9AkqVV4T8",
-  authDomain: "expensify-28fc5.firebaseapp.com",
-  databaseURL: "https://expensify-28fc5.firebaseio.com",
-  projectId: "expensify-28fc5",
-  storageBucket: "expensify-28fc5.appspot.com",
-  messagingSenderId: "654114452965"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 import 'react-dates/initialize';
+import DotEnv from 'dotenv';
+
+
+DotEnv.config({ path: '.env.test'});
 
 Enzyme.configure({
   adapter: new Adapter()
